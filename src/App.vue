79f870a7e269309e06c8b1b40a9b189c12624c40
Ashutosh <ashusingh15673@gmail.com>
2021-11-14 18:27:12 +0530

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+/* eslint-disable vue/no-unused-components */
+<template>
+  <h1>Vue TinyMCE Example</h1>
+  <editor
+    api-key= "8wphbjzfrlwugpe3ibkx678fsntv1nhp05ze8jokrqspn4l9io"
+    :init="{
+      height: 500,
+      menubar: false,
+      plugins: ['image '],
+    }"
+  />
+</template>
+
+<script>
+import Editor from "@tinymce/tinymce-vue";
+
+export default {
+  name: "App",
+  components: {
+    editor: Editor,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
